ea9f30920e07f7fb268ab2fab882ce06717c7bf9
vli21 <vli21@stuy.edu>
2021-01-10 13:57:21 -0500

    i have friends now

--- frontend/src/FriendsList.js
+++ frontend/src/FriendsList.js
@@ -11,7 +11,14 @@ const FriendsList = () => {
 
       <Box width={300} height= "100%" bgcolor="text.primary" color="primary.contrastText" p={2}>
         Friends List
+        <br/>
+        Friends
+        <br/>
+        Friends
+        <br/>
+        Friends
       </Box>
+
     </Grid>
     </>
   );
