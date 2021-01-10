47030a36a76e14b4ebb8eee462775d826198b7a6
ESGit3 <ethanshan706@gmail.com>
2021-01-09 20:06:12 -0500

    Home.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import Button from "@material-ui/core/Button/Button";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant={"contained"}>Click Me!</Button>
+        <Home />
       </header>
     </div>
   );
---
+++ frontend/src/Home.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import Button from "@material-ui/core/Button/Button";
+
+const Home = () => {
+  return (
+    <>
+      <Button>Hello world</Button>
+      <Button variant={"contained"}>Hello world</Button>
+      <Button color={"primary"}>Hello world</Button>
+    </>
+  );
+};
+
+export default Home;
