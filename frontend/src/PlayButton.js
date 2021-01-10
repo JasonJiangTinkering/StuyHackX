10a94add5bf029ff4a27f30dcb26695162ab8e25
vli21 <vli21@stuy.edu>
2021-01-09 23:38:04 -0500

    play button fixed

---
+++ frontend/src/PlayButton.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const PlayButton = () => {
+  return (
+    <>
+      <Button>Default</Button>
+    </>
+  );
+};
+
+export default PlayButton;
