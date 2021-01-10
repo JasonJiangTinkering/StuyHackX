b77efacea4fb9df4fa66c2146449cefad0b6f870
jasonjiangstuy <jasonjiangny@gmail.com>
2021-01-09 23:14:21 -0500

    COmbine frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import React from "react";
 import Home from "./Home";
 
+import Login from "./Login";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <table>
+      <tr>
+        <div className="App">
+          <header className="App-header">
         <Home />
-      </header>
-    </div>
+          </header>
+        </div>
+      </tr>
+    </table>
   );
 }
 
