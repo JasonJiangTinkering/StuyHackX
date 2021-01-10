99a382618f3f28b40b40f8639cfc2b8701f7a54e
vli21 <vli21@stuy.edu>
2021-01-10 00:40:28 -0500

    saved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,7 @@ function App() {
       <header className="App-header">
         <ButtonAppBar/>
       </header>
-<<<<<<< HEAD
       <PlayButton/>
-=======
-      <PlayButton />
->>>>>>> a4bb7e6b22121fc537f3a000bbd4dd00550bf062
     </div>
   );
 }
--- frontend/src/PlayButton.js
+++ frontend/src/PlayButton.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import "./App.css";
-=======
->>>>>>> a4bb7e6b22121fc537f3a000bbd4dd00550bf062
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -9,19 +5,11 @@ import Button from '@material-ui/core/Button';
 const PlayButton = () => {
   return (
     <>
-<<<<<<< HEAD
-      <Button>Default</Button>
-=======
     <center>
       <Button>Default</Button>
       </center>
->>>>>>> a4bb7e6b22121fc537f3a000bbd4dd00550bf062
     </>
   );
 };
 
-<<<<<<< HEAD
 export default PlayButton;
-=======
-export default PlayButton;
->>>>>>> a4bb7e6b22121fc537f3a000bbd4dd00550bf062
