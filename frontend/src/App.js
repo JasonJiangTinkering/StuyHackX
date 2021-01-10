de2c8570870080b1eabe559a90586d1c98eeedc2
vli21 <vli21@stuy.edu>
2021-01-09 20:55:22 -0500

    AfterLogin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import React from "react";
 import Home from "./Home";
+import AfterLogin from "./AfterLogin";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Home />
+        <AfterLogin/>
       </header>
     </div>
   );
