01c2aea2a06eaf8d84e81b1cebfd2750da867680
ESGit3 <ethanshan706@gmail.com>
2021-01-09 19:46:08 -0500

    added material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import Button from "@material-ui/core/Button/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant={"contained"}>Click Me!</Button>
       </header>
     </div>
   );
