0c1e1d37d5ef1b8a1aea4034d3bcf3428011dbc7
esteakshapin(pop_os) <esteakshapin@gmail.com>
2021-01-09 22:53:02 -0500

    user authentication done

--- django/src/users/views.py
+++ django/src/users/views.py
@@ -1,6 +1,7 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from rest_auth.registration.views import SocialLoginView
+from rest_auth.registration.serializers import SocialLoginSerializer
 from rest_framework import viewsets
 from users.models import CustomUser
 from users.serializers import CustomUserSerializer
@@ -8,7 +9,15 @@ from users.serializers import CustomUserSerializer
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    # client_class = OAuth2Client
+    client_class = OAuth2Client
+    callback_url = 'localhost:8000'
+    serializer_class = SocialLoginSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        serializer_class = self.get_serializer_class()
+        kwargs['context'] = self.get_serializer_context()
+        return serializer_class(*args, **kwargs)
+
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
